f9796973097f58bfa5c951666cf715ee1b1e3425
adityachavan <adityachavan@nimapinfotech.com>
2023-04-23 23:22:52 +0530

    updated env

--- config/envVars.js
+++ config/envVars.js
@@ -13,11 +13,6 @@ switch (NODE_ENV) {
     envVars.SALT_ROUNDS = 10;
     envVars.JWT_SECRET = process.env.JWT_SECRET || 'thisissupersecretpassword';
     envVars.JWT_EXPIRY = process.env.JWT_EXPIRY || 24;
-    envVars.FORGOT_PASSWORD_TOKEN_EXPIRY = process.env.FORGOT_PASSWORD_TOKEN_EXPIRY || 15;
-    envVars.MAIL_HOST = process.env.MAIL_HOST;
-    envVars.MAIL_PORT = process.env.MAIL_PORT;
-    envVars.MAIL_USER = process.env.MAIL_USER;
-    envVars.MAIL_PASS = process.env.MAIL_PASS;
     break;
 
   case 'development':
@@ -28,11 +23,6 @@ switch (NODE_ENV) {
     envVars.SALT_ROUNDS = 10;
     envVars.JWT_SECRET = 'thisissupersecretpassword';
     envVars.JWT_EXPIRY = 24;
-    envVars.FORGOT_PASSWORD_TOKEN_EXPIRY = 15;
-    envVars.MAIL_HOST = process.env.MAIL_HOST || 'smtp.gmail.com';
-    envVars.MAIL_PORT = process.env.MAIL_PORT || 465;
-    envVars.MAIL_USER = process.env.MAIL_USER || 'jobportalapptest@gmail.com';
-    envVars.MAIL_PASS = process.env.MAIL_PASS || 'lxerunlchehksmma';
     break;
 
   default:
@@ -43,11 +33,6 @@ switch (NODE_ENV) {
     envVars.JWT_SECRET = 'thisissupersecretpassword';
     envVars.SALT_ROUNDS = 10;
     envVars.JWT_EXPIRY = 24;
-    envVars.FORGOT_PASSWORD_TOKEN_EXPIRY = 15;
-    envVars.MAIL_HOST = process.env.MAIL_HOST || 'smtp.ethereal.email';
-    envVars.MAIL_PORT = process.env.MAIL_PORT || 587;
-    envVars.MAIL_USER = process.env.MAIL_USER;
-    envVars.MAIL_PASS = process.env.MAIL_PASS;
 }
 
 module.exports = envVars;
